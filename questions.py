eb18faad6e00ac4c8da4296b310d78528dd7f106
catagaticaf <cata.gatica.f@hotmail.com.ar>
2025-03-18 19:30:50 -0300

    ej7.3: questions to ask

--- questions.py
+++ questions.py
@@ -24,14 +24,15 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+# combino las 3 listas en una sola lista:
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 puntaje = 0 #para llevar el registro del puntaje
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
-    # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+for question, answer_options, correct_answer_index in questions_to_ask:
+    # se muestra la pregunta
+    print(question)
+    for i, answer in enumerate(answer_options):
+        #imprimo las opciones de respuesta
         print(f"{i + 1}. {answer}")
 # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -41,9 +42,9 @@ for _ in range(3):
             #le resto 1 para respetar el inicio del indice en 0
             user_answer = int(user_answer) - 1
             #checkeo que el numero ingresado este dentro del rango permitido
-            if 0 <= user_answer < len(answers[question_index]):
+            if 0 <= user_answer < len(answer_options):
                 # Se verifica si la respuesta es correcta
-                if user_answer == correct_answers_index[question_index]:
+                if user_answer == correct_answer_index:
                     print("¡Correcto!")
                     puntaje += 1
                     break
@@ -63,7 +64,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(answer_options[correct_answer_index])
     # Se imprime un blanco al final de la pregunta
     print()
 print(f'el puntaje obtenido fue: {puntaje}')
